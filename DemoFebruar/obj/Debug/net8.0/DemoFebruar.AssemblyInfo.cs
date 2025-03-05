e19b22dd70d880657a8e29308f76bae2340d3676
IMalygosI <dmitriy.udaltcov@gmail.com>
2025-03-05 17:29:16 +0300

    05.03.2025

--- DemoFebruar/obj/Debug/net8.0/DemoFebruar.AssemblyInfo.cs
+++ DemoFebruar/obj/Debug/net8.0/DemoFebruar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoFebruar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a5532d5003c14a06d030d80178ab6d54fde448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4de1bafd29066cbeddba61bf1a6ecef64cf85d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoFebruar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoFebruar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
